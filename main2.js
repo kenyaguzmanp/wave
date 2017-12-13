2cd49728712d1b029e9e453d0d86f33717e103c7
kenya.guzman@mediamonks.com <kenya.guzman@mediamonks.com>
2017-12-13 11:43:21 -0300

    put some code as functions

--- main2.js
+++ main2.js
@@ -1,48 +1,81 @@
-var audioCtx = new AudioContext();
 
+//audioCtx is a constant
+var audioCtx = new AudioContext();
 //Create audio source
-//Here, we use an audio file, but this could also be e.g. microphone input
+//The source is an audio file
+//the audio element is a constant
 var audioEle = new Audio();
 audioEle.src = 'audio-sample.mp3';//insert file name here
 audioEle.autoplay = true;
 audioEle.preload = 'auto';
+//audioSourceNode is a constant
 var audioSourceNode = audioCtx.createMediaElementSource(audioEle);
 
-//Create analyser node
-var analyserNode = audioCtx.createAnalyser();
-analyserNode.fftSize = 256;
-var bufferLength = analyserNode.frequencyBinCount;
-console.log("bufferLength " + bufferLength);
-var dataArray = new Float32Array(bufferLength);
-//console.log("dataArray: " , dataArray);
+var analyserNode;
+var bufferLength;
+var dataArray=[];
 
-//Set up audio node network
-audioSourceNode.connect(analyserNode);
-analyserNode.connect(audioCtx.destination);
+var canvas;
+var canvasCtx;
+
+createAnalyserNode();
+
+
+
+
+//Create analyser node
+function createAnalyserNode(){
+  console.log("in create analyser Node");
+  analyserNode = audioCtx.createAnalyser();
+  console.log("analyserNode: " , analyserNode);
+  analyserNode.fftSize = 256;
+  bufferLength = analyserNode.frequencyBinCount;
+  console.log("bufferLength " + bufferLength);
+  dataArray = new Float32Array(bufferLength);
+
+  dataSampleArray = new Float32Array(bufferLength);
+  //Set up audio node network
+  audioSourceNode.connect(analyserNode);
+  analyserNode.connect(audioCtx.destination);
+  /*
+  analyserNode.getFloatTimeDomainData(dataSampleArray);
+  console.log("sampleValues: " + dataSampleArray);
+  */
+  createCanvas();
+ 
+}
 
 //Create 2D canvas
-var canvas = document.createElement('canvas');
-canvas.style.position = 'absolute';
-canvas.style.top = 0;
-canvas.style.left = 0;
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-document.body.appendChild(canvas);
-var canvasCtx = canvas.getContext('2d');
-canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-console.log("canvas height: " + canvas.height);  
+function createCanvas(){
+  console.log("in create canvas");
+  canvas = document.createElement('canvas');
+  canvas.style.position = 'absolute';
+  canvas.style.top = 0;
+  canvas.style.left = 0;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  document.body.appendChild(canvas);
+  canvasCtx = canvas.getContext('2d');
+  canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+  //draw the canvas
+  draw(); 
+}
+
+ 
 
 //posX Array 
 var posXarray=[];
 //initialize
 posXarray[0]=0;
+
+
 function draw() {
   //Schedule next redraw
   requestAnimationFrame(draw);
 
   //Get spectrum data
-   analyserNode.getFloatFrequencyData(dataArray);
-
+  analyserNode.getFloatFrequencyData(dataArray);
+  //console.log("datArray[0] en draw " + dataArray[0]);
 
   //Draw black background
   canvasCtx.fillStyle = 'rgb(0, 0, 0)';
@@ -57,10 +90,10 @@ function draw() {
     canvasCtx.fillRect(posX, canvas.height - barHeight / 2, barWidth, barHeight / 2);
     //space between bars
     posX += barWidth + 8;
-    posXarray.push(posX);
+    //posXarray.push(posX);
     //console.log("posX: " + posX);
   }
   //console.log("posXarray: " + posXarray);
 };
 
-draw();
+//draw();
