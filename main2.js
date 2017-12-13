c23908d798ca3a274b79b912f815b3877d5a536b
kenya.guzman@mediamonks.com <kenya.guzman@mediamonks.com>
2017-12-13 13:26:24 -0300

    added the bezier curve to rectangles

--- main2.js
+++ main2.js
@@ -54,11 +54,13 @@ function createCanvas(){
   canvas.style.left = 0;
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  console.log("canvas height: " + canvas.height + "canvas width: " + canvas.width);
   document.body.appendChild(canvas);
   canvasCtx = canvas.getContext('2d');
   canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
   //draw the canvas
-  draw(); 
+  draw();
+  
 }
 
  
@@ -76,24 +78,52 @@ function draw() {
   //Get spectrum data
   analyserNode.getFloatFrequencyData(dataArray);
   //console.log("datArray[0] en draw " + dataArray[0]);
+  //analyserNode.getFloatTimeDomainData(dataSampleArray);
+  //console.log("sampleValues: " + dataSampleArray.length);
 
   //Draw black background
   canvasCtx.fillStyle = 'rgb(0, 0, 0)';
   canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
   
   //Draw spectrum
-  var barWidth = (canvas.width / bufferLength) * 2;
+  var barWidth = (canvas.width / bufferLength)*7; 
   var posX = 0;
+  var posX2 = posX + barWidth;
+  //console.log("BUFFER LENGTH: " + bufferLength + "barWidth: " + barWidth);
+
   for (var i = 0; i < bufferLength; i++) {
     var barHeight = (dataArray[i]+130) * 2;
+    
     canvasCtx.fillStyle = 'rgb(' + Math.floor(barHeight + 100) + ', 50, 50)';
     canvasCtx.fillRect(posX, canvas.height - barHeight / 2, barWidth, barHeight / 2);
+    //canvasCtx.fillRect(posX, canvas.height - barHeight / 2, 2, 2);
+    //draw the left corner  down point of every bar
+    canvasCtx.fillStyle = 'yellow';
+    canvasCtx.fillRect(posX, canvas.height - 5, barWidth/4, 5);
+
+    canvasCtx.fillStyle = 'white';
+    canvasCtx.fillRect(posX2-4, canvas.height - 5, barWidth/4, 5);
+
+    canvasCtx.lineWidth = 1;
+    canvasCtx.strokeStyle = 'white';
+    canvasCtx.beginPath();
+    //initial point
+    canvasCtx.moveTo(posX, canvas.height - 5);
+    canvasCtx.bezierCurveTo(posX+17, canvas.height - barHeight / 2, posX2 - 17, canvas.height - barHeight / 2, posX2, canvas.height - 5);
+    canvasCtx.stroke();
+
+
     //space between bars
     posX += barWidth + 8;
+
+    posX2 += barWidth + 8;
     //posXarray.push(posX);
     //console.log("posX: " + posX);
+
+    
+
   }
   //console.log("posXarray: " + posXarray);
 };
-
+ 
 //draw();
