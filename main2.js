64d7dc3652ef6f5bbd0e8d531d91bc1d6024249e
kenya.guzman@mediamonks.com <kenya.guzman@mediamonks.com>
2017-12-13 15:18:34 -0300

    move all waves up

--- main2.js
+++ main2.js
@@ -49,11 +49,15 @@ function createAnalyserNode(){
 function createCanvas(){
   console.log("in create canvas");
   canvas = document.createElement('canvas');
-  canvas.style.position = 'absolute';
-  canvas.style.top = 0;
-  canvas.style.left = 0;
+  //canvas.style.position = 'absolute';
+  //canvas.style.top = 0;
+  //canvas.style.left = 0;
+  /*
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  */
+  canvas.width = 800;
+  canvas.height = 600;
   console.log("canvas height: " + canvas.height + "canvas width: " + canvas.width);
   document.body.appendChild(canvas);
   canvasCtx = canvas.getContext('2d');
@@ -86,34 +90,40 @@ function draw() {
   canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
   
   //Draw spectrum
-  var barWidth = (canvas.width / bufferLength)*7; 
+  //barWidth also can be used to set the numbers of waves 
+  var barWidth = (canvas.width / bufferLength)*9;  
   var posX = 0;
   var posX2 = posX + barWidth;
   //console.log("BUFFER LENGTH: " + bufferLength + "barWidth: " + barWidth);
   var posY2 = canvas.height - 5;
+  var posY2Up = canvas.height/2;
   //epsilon is kindda the amplitude
   var epsilon = 20;
 
   for (var i = 0; i < bufferLength; i++) {
-    var barHeight = (dataArray[i]+130) * 2;
+    var normalizedDataArray = dataArray[i]+130;
+    var barHeight = normalizedDataArray * 5;
+    //var barHeight = (dataArray[i]+130) * 5;
     var posY = canvas.height - barHeight / 2;
-    
+    var posYUp = posY - 200;
+
     canvasCtx.fillStyle = 'rgb(' + Math.floor(barHeight + 100) + ', 50, 50)';
-    canvasCtx.fillRect(posX, posY, barWidth, barHeight / 2);
-    //canvasCtx.fillRect(posX, canvas.height - barHeight / 2, 2, 2);
+    //canvasCtx.fillRect(posX, posY, barWidth, barHeight / 2);
+    canvasCtx.fillRect(posX, posYUp, barWidth, barHeight / 2);
+   
     //draw the left corner  down point of every bar
     canvasCtx.fillStyle = 'yellow';
-    canvasCtx.fillRect(posX, posY2, barWidth/4, 5);
+    canvasCtx.fillRect(posX, posY2/2, barWidth/4, 5);
 
     canvasCtx.fillStyle = 'white';
-    canvasCtx.fillRect(posX2-4, posY2, barWidth/4, 5);
-
+    canvasCtx.fillRect(posX2-4, posY2Up, barWidth/4, 5);
+    //bezier curves
     canvasCtx.lineWidth = 1;
     canvasCtx.strokeStyle = 'white';
     canvasCtx.beginPath();
     //initial point
-    canvasCtx.moveTo(posX, posY2);
-    canvasCtx.bezierCurveTo(posX+epsilon, posY, posX2 - epsilon, posY, posX2, posY2);
+    canvasCtx.moveTo(posX, posY2Up);
+    canvasCtx.bezierCurveTo(posX+epsilon, posYUp, posX2 - epsilon, posYUp, posX2, posY2Up);
     canvasCtx.stroke();
 
 
